ae767c47333ad970c914ee55dc1a49e199250eb4
Gsuslov <91145537+Gsuslov@users.noreply.github.com>
2021-10-06 23:45:39 +0300

    Add Controllers

---
+++ Controllers/User.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace site.Controllers
+{
+    [ApiController]
+    [Route("/User")]
+    public class User : ControllerBase
+
+
+    {
+
+        [HttpGet("SeatchRiver")]
+        public string SeatchRiver(string str)
+        {
+            return str; // Поиск водоема
+
+        }
+
+        [HttpGet("RiverInfo")]
+        public string RiverInfo(string str)
+        {
+            return str; // Просмотр информации о водоеме
+        }
+
+        [HttpPost("CreateComments")]
+        public string CreateComments(string str)
+        {
+            return str; // Создание коментария
+        }
+
+        [HttpGet("Comments")]
+        public string Comments(string str)
+        {
+            return str; // Комментирование
+        }
+
+      
+        [HttpGet("LivingFish")]
+        public string LivingFish(string str)
+        {
+            return str; // желаемая рыба
+        }
+
+        [HttpGet("Region")]
+        public string Region(string str)
+        {
+            return str; // Выбор региона
+        }
+    }
+
+}
---
+++ FishHead.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace site
+{
+    public class FishHead
+    {
+        public string Fish { get; set; }
+        public string River { get; set; }
+        public string Comments { get; set; }
+        public string RiverInfo { get; set; }
+    }
+}
